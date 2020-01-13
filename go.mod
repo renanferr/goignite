caddf6b43cc370f8c6ae3414e9113434a0b11292
João Paulo Faria <joao.faria@b2wdigital.com>
2020-01-13 18:51:51 -0300

    adding juju errors

--- go.mod
+++ go.mod
@@ -3,22 +3,27 @@ module github.com/jpfaria/goignite
 go 1.13
 
 require (
-	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/h2non/filetype v1.0.10 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-playground/validator/v10 v10.1.0
 	github.com/jpfaria/logrus-redis-hook v1.0.2
+	github.com/juju/errors v0.0.0-20190930114154-d42613fe1ab9
+	github.com/juju/loggo v0.0.0-20190526231331-6e530bcce5d8 // indirect
+	github.com/juju/testing v0.0.0-20191001232224-ce9dec17d28b // indirect
 	github.com/knadh/koanf v0.6.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
-	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/neko-neko/echo-logrus v1.1.0
 	github.com/ravernkoh/cwlogsfmt v0.0.0-20180121032441-917bad983b4c
 	github.com/sirupsen/logrus v1.4.0
 	github.com/spf13/pflag v1.0.3
-	github.com/spf13/viper v1.5.0 // indirect
 	github.com/stretchr/testify v1.4.0
 	github.com/valyala/fasttemplate v1.1.0 // indirect
-	gopkg.in/go-playground/validator.v9 v9.30.2
+	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/resty.v1 v1.12.0
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- pkg/http/server/echo/parser/json-response.go
+++ pkg/http/server/echo/parser/json-response.go
@@ -3,10 +3,10 @@ package parser
 import (
 	"net/http"
 
-	"github.com/jpfaria/goignite/pkg/errors"
+	"github.com/go-playground/validator/v10"
 	"github.com/jpfaria/goignite/pkg/http/server/model/response"
+	"github.com/juju/errors"
 	"github.com/labstack/echo"
-	"gopkg.in/go-playground/validator.v9"
 )
 
 func JSONResponse(c echo.Context, code int, i interface{}, err error) error {
@@ -22,40 +22,44 @@ func JSONResponse(c echo.Context, code int, i interface{}, err error) error {
 
 func JSONErrorResponse(c echo.Context, err error) error {
 
-	switch t := err.(type) {
-	default:
-		return c.JSONPretty(http.StatusInternalServerError, response.ErrorResponse{HttpStatusCode: http.StatusInternalServerError, Message: t.Error()}, "  ")
-	case *errors.NotFoundError:
-		return c.JSONPretty(http.StatusNotFound, response.ErrorResponse{HttpStatusCode: http.StatusNotFound, Message: t.Error()}, "  ")
-	case *errors.IllegalArgumentError:
-		return c.JSONPretty(http.StatusBadRequest, response.ErrorResponse{HttpStatusCode: http.StatusBadRequest, Message: t.Error()}, "  ")
-	case validator.ValidationErrors:
+	if errors.IsNotFound(err) {
+		return c.JSONPretty(http.StatusNotFound, response.ErrorResponse{HttpStatusCode: http.StatusNotFound, Message: err.Error()}, "  ")
+	} else if errors.IsNotValid(err) {
+		return c.JSONPretty(http.StatusBadRequest, response.ErrorResponse{HttpStatusCode: http.StatusBadRequest, Message: err.Error()}, "  ")
+	} else {
 
-		var fe validator.FieldError
-		var verrs []response.ValidationErrorResponse
+		switch t := err.(type) {
+		default:
+			return c.JSONPretty(http.StatusInternalServerError, response.ErrorResponse{HttpStatusCode: http.StatusInternalServerError, Message: t.Error()}, "  ")
+		case validator.ValidationErrors:
 
-		for i := 0; i < len(t); i++ {
+			var fe validator.FieldError
+			var verrs []response.ValidationErrorResponse
 
-			fe = t[i].(validator.FieldError)
+			for i := 0; i < len(t); i++ {
 
-			verr := response.ValidationErrorResponse{
-				FieldName: fe.Field(),
-				Message:   fe.Namespace(),
+				fe = t[i].(validator.FieldError)
+
+				verr := response.ValidationErrorResponse{
+					FieldName: fe.Field(),
+					Message:   fe.Namespace(),
+				}
+
+				verrs = append(verrs, verr)
 			}
 
-			verrs = append(verrs, verr)
-		}
-
-		return c.JSONPretty(
-			http.StatusUnprocessableEntity,
-			response.UnprocessableEntityErrorResponse{
-				ErrorResponse: response.ErrorResponse{
-					HttpStatusCode: http.StatusUnprocessableEntity,
-					Message:        "The server understands the content type of the request entity but was unable to process the contained instructions.",
+			return c.JSONPretty(
+				http.StatusUnprocessableEntity,
+				response.UnprocessableEntityErrorResponse{
+					ErrorResponse: response.ErrorResponse{
+						HttpStatusCode: http.StatusUnprocessableEntity,
+						Message:        "The server understands the content type of the request entity but was unable to process the contained instructions.",
+					},
+					ValidationErrors: verrs,
 				},
-				ValidationErrors: verrs,
-			},
-			"  ",
-		)
+				"  ",
+			)
+		}
 	}
+
 }

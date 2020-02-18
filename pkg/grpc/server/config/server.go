97c855858366f448a7e57dd37ba4acf6910f55e9
João Paulo Faria <joao.faria@b2wdigital.com>
2020-02-18 03:05:54 -0300

    fixing grpc configs

--- pkg/grpc/server/config/server.go
+++ pkg/grpc/server/config/server.go
@@ -6,13 +6,15 @@ import (
 	"github.com/jpfaria/goignite/pkg/config"
 )
 
-const Port = "server.grpc.port"
-const MaxConcurrentStreams = "server.grpc.maxConcurrentStreams"
+const (
+	Port                 = "grpc.server.port"
+	MaxConcurrentStreams = "grpc.server.maxconcurrentstreams"
+)
 
 func init() {
 
 	log.Println("getting configurations for grpc server")
 
-	config.SetInt(Port, 9090, "server grpc port")
-	config.SetUint64(MaxConcurrentStreams, 5000, "server grpc max concurrent streams")
+	config.Add(Port, 9090, "server grpc port")
+	config.Add(MaxConcurrentStreams, 5000, "server grpc max concurrent streams")
 }

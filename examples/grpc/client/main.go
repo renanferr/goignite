7da71c50f96a18fde467eafc34e9e77063d91c20
João Paulo Faria <jpfaria@gmail.com>
2020-03-04 02:14:21 -0300

    adding grpc client and server examples

---
+++ examples/grpc/client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+
+	"github.com/b2wdigital/goignite/pkg/config"
+	"github.com/b2wdigital/goignite/pkg/grpc/client"
+	"github.com/b2wdigital/goignite/pkg/grpc/client/model"
+	"github.com/b2wdigital/goignite/pkg/log/logrus"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	config.Parse()
+
+	logrus.Start()
+
+	log := logrus.FromContext(ctx)
+
+	request := &TestRequest{
+		Message: "mensagem da requisição",
+	}
+
+	options := model.OptionsBuilder.
+		Host("localhost").
+		Port(9090).
+		Build()
+
+	conn := client.NewClient(ctx, &options)
+	defer conn.Close()
+
+	c := NewExampleClient(conn)
+
+	test, err := c.Test(ctx, request)
+	if err != nil {
+		log.Fatalf("%v.Call(_) = _, %v", c, err)
+	}
+
+	log.Info(test.Message)
+}
---
+++ examples/grpc/server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/b2wdigital/goignite/pkg/config"
+	"github.com/b2wdigital/goignite/pkg/grpc/server"
+	"github.com/b2wdigital/goignite/pkg/log/logrus"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	err := config.Parse()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logrus.Start()
+
+	s := server.Start(ctx)
+
+	RegisterExampleServer(s, &Service{})
+
+	server.Serve(ctx)
+}
+
+
+type Service struct {
+}
+
+func (h *Service) Test(ctx context.Context, request *TestRequest) (*TestResponse, error) {
+
+	l := logrus.FromContext(ctx)
+
+	l.Info(request.Message)
+
+	return &TestResponse{Message: "hello world"}, nil
+}

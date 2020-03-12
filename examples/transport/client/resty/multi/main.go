b73e542a338349f84f56acc5dfe804b3a02d6e48
João Paulo Faria <jpfaria@gmail.com>
2020-03-12 14:39:04 -0300

    adding example for resty with multi clients
    closes #16

---
+++ examples/transport/client/resty/multi/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+
+	"github.com/b2wdigital/goignite/pkg/config"
+	"github.com/b2wdigital/goignite/pkg/log"
+	"github.com/b2wdigital/goignite/pkg/log/logrus/v1"
+	"github.com/b2wdigital/goignite/pkg/transport/client/resty/v2"
+)
+
+func main() {
+
+	var err error
+
+	config.Load()
+
+	ctx := context.Background()
+
+	log.NewLogger(logrus.NewLogger())
+
+	l := log.FromContext(ctx)
+
+	// call google
+	
+	googleopt := new(resty.Options)
+
+	err = config.UnmarshalWithPath("app.client.resty.google", googleopt)
+	if err != nil {
+		l.Errorf(err.Error())
+	}
+
+	cligoogle := resty.NewClient(ctx, googleopt)
+	reqgoogle := cligoogle.R()
+
+	respgoogle, err := reqgoogle.Get("/")
+	if err != nil {
+		l.Fatalf(err.Error())
+	}
+
+	l.Infof(respgoogle.String())
+
+	// call acom
+
+	acomopt := new(resty.Options)
+
+	err = config.UnmarshalWithPath("app.client.resty.acom", acomopt)
+	if err != nil {
+		l.Errorf(err.Error())
+	}
+
+	cliacom := resty.NewClient(ctx, acomopt)
+	reqacom := cliacom.R()
+
+	respacom, err := reqacom.Get("/")
+	if err != nil {
+		l.Fatalf(err.Error())
+	}
+
+	l.Infof(respacom.String())
+}

163c962d1cac3e43e57c8f89a8b296a4f8bff6c8
João Paulo Faria <jpfaria@gmail.com>
2020-03-20 12:05:52 -0300

    refactoring cloudevents nats

---
+++ pkg/transport/client/cloudevents/v1/nats/sender/client.go
@@ -0,0 +1,17 @@
+package sender
+
+import (
+	l "github.com/b2wdigital/goignite/pkg/log"
+	"github.com/cloudevents/sdk-go/pkg/cloudevents/client"
+	cloudeventsnats "github.com/cloudevents/sdk-go/pkg/cloudevents/transport/nats"
+)
+
+func NewClient(url string, subject string) (client.Client, error){
+
+	t, err := cloudeventsnats.New(url, subject)
+	if err != nil {
+		l.Fatalf("failed to create transport: %s", err.Error())
+	}
+
+	return client.New(t)
+}

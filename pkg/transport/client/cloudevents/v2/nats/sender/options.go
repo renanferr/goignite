8228ad31423152ac097863ce8d31018ad2f38161
João Paulo Faria <jpfaria@gmail.com>
2020-03-31 19:41:16 -0300

    adding implementation for cloudevents v2

---
+++ pkg/transport/client/cloudevents/v2/nats/sender/options.go
@@ -0,0 +1,39 @@
+package sender
+
+import (
+	"github.com/b2wdigital/goignite/pkg/config"
+	"github.com/lann/builder"
+)
+
+type Options struct {
+	Url      string
+	Subjects []string
+}
+
+type optionsBuilder builder.Builder
+
+func (b optionsBuilder) Url(value string) optionsBuilder {
+	return builder.Set(b, "Url", value).(optionsBuilder)
+}
+
+func (b optionsBuilder) Subjects(value []string) optionsBuilder {
+	return builder.Set(b, "Subjects", value).(optionsBuilder)
+}
+
+func (b optionsBuilder) Build() Options {
+	return builder.GetStruct(b).(Options)
+}
+
+var OptionsBuilder = builder.Register(optionsBuilder{}, Options{}).(optionsBuilder)
+
+func DefaultOptions() (*Options, error) {
+
+	o := &Options{}
+
+	err := config.UnmarshalWithPath("transport.client.cloudevents.nats.sender", o)
+	if err != nil {
+		return nil, err
+	}
+
+	return o, nil
+}

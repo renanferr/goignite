8f47d9279e2379e5e60571cec07bf2d21fcc4c5c
João Paulo Faria <jpfaria@gmail.com>
2020-05-07 03:01:06 -0300

    adding sql oracle godror client
    closes #59

---
+++ pkg/transport/client/godror/v0/config.go
@@ -0,0 +1,27 @@
+package elasticsearch
+
+import (
+	"time"
+
+	"github.com/b2wdigital/goignite/pkg/config"
+
+	"log"
+)
+
+const (
+	DataSourceName  = "transport.client.godror.datasourcename"
+	ConnMaxLifetime = "transport.client.godror.connmaxlifetime"
+	MaxIdleConns    = "transport.client.godror.maxidleconns"
+	MaxOpenConns    = "transport.client.godror.maxopenconns"
+)
+
+func init() {
+
+	log.Println("getting configurations for oracle (godror)")
+
+	config.Add(DataSourceName, "", "database name and connection information")
+	config.Add(ConnMaxLifetime, 0*time.Second, "sets the maximum amount of time a connection may be reused. If d <= 0, connections are reused forever")
+	config.Add(MaxIdleConns, 2, "sets the maximum number of connections in the idle connection pool.")
+	config.Add(MaxOpenConns, 5, "sets the maximum number of open connections to the database.")
+
+}

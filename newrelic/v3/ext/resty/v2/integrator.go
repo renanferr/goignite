6685745f4cdd4f50667d1a21e7673277501b2e2a
João Paulo Faria <jpfaria@gmail.com>
2020-09-19 02:22:42 -0300

    fix: fixing resty integrator

--- newrelic/v3/ext/resty/v2/integrator.go
+++ newrelic/v3/ext/resty/v2/integrator.go
@@ -1,6 +1,10 @@
 package giresty
 
 import (
+	"context"
+
+	"net/http"
+
 	gieventbus "github.com/b2wdigital/goignite/eventbus"
 	gilog "github.com/b2wdigital/goignite/log"
 	giresty "github.com/b2wdigital/goignite/resty/v2"
@@ -26,13 +30,32 @@ func (i *Integrator) Integrate(client *resty.Client) error {
 
 		client.OnBeforeRequest(func(client *resty.Client, request *resty.Request) error {
 
-			logger := gilog.FromContext(request.Context())
+			rctx := request.Context()
 
-			txn := newrelic.FromContext(request.Context())
+			logger := gilog.FromContext(rctx)
 
+			txn := newrelic.FromContext(rctx)
 			txn.InsertDistributedTraceHeaders(request.Header)
 
+			req, _ := http.NewRequest(request.Method, client.HostURL, nil)
+			req.Header = request.Header
+
+			s := newrelic.StartExternalSegment(txn, req)
+			ctx := context.WithValue(rctx, "nrext", s)
+
+			request.SetContext(ctx)
+
 			logger.Debugf("rest request processing")
+
+			return nil
+		})
+
+		client.OnAfterResponse(func(c *resty.Client, resp *resty.Response) error {
+
+			ctx := resp.Request.Context()
+
+			s := ctx.Value("nrext").(*newrelic.ExternalSegment)
+			s.End()
 
 			return nil
 		})

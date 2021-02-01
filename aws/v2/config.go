6e22740fe6ce342137e05014353f2be47add9222
barbosaigor <igor.barbosa1920@gmail.com>
2021-02-01 11:33:29 -0300

    feat(aws): allow aws retryer configuration

--- aws/v2/config.go
+++ aws/v2/config.go
@@ -7,11 +7,15 @@ import (
 )
 
 const (
-	Key             = "aws.access.key.id"
-	Secret          = "aws.secret.access.key"
-	Region          = "aws.default.region"
-	Session         = "aws.session.token"
-	CustomEndpoint  = "aws.custom.endpoint"
+	Key            = "aws.access.key.id"
+	Secret         = "aws.secret.access.key"
+	Region         = "aws.default.region"
+	Session        = "aws.session.token"
+	CustomEndpoint = "aws.custom.endpoint"
+
+	Retryer             = "aws.custom.retryer"
+	RetryerMaxAttempts  = Retryer + ".maxattempts"
+	RetryerHasRateLimit = Retryer + ".hasratelimit"
 )
 
 func init() {
@@ -24,4 +28,6 @@ func init() {
 	giconfig.Add(Session, "", "defines the aws session token")
 	giconfig.Add(CustomEndpoint, false, "defines if should point to localhost")
 
+	giconfig.Add(RetryerMaxAttempts, 5, "defines max attempts for rate limit")
+	giconfig.Add(RetryerHasRateLimit, true, "defines if retryer has rate limit")
 }

8b7e70033b5abd6955f2064dcb8afd0fa297e59f
João Paulo Faria <jpfaria@gmail.com>
2021-03-02 02:06:10 -0300

    feat(fiber): adds tid middleware

---
+++ fiber/v2/ext/tid/config.go
@@ -0,0 +1,19 @@
+package tid
+
+import (
+	giconfig "github.com/b2wdigital/goignite/config"
+	gifiber "github.com/b2wdigital/goignite/fiber/v2"
+)
+
+const (
+	root    = gifiber.ExtRoot + ".tid"
+	enabled = root + ".enabled"
+)
+
+func init() {
+	giconfig.Add(enabled, true, "enable/disable tid middleware")
+}
+
+func isEnabled() bool {
+	return giconfig.Bool(enabled)
+}
---
+++ fiber/v2/ext/tid/middleware.go
@@ -0,0 +1,35 @@
+package tid
+
+import (
+	"context"
+
+	"github.com/b2wdigital/goignite/info"
+	"github.com/gofiber/fiber/v2"
+	uuid "github.com/satori/go.uuid"
+)
+
+func Middleware(ctx context.Context, app *fiber.App) error {
+
+	if !isEnabled() {
+		return nil
+	}
+
+	app.Use(middleware)
+
+	return nil
+}
+
+func middleware(config ...fiber.Config) fiber.Handler {
+
+	// Return new handler
+	return func(c *fiber.Ctx) error {
+
+		tid := c.Get("X-TID", info.AppName+"-"+uuid.NewV4().String())
+
+		c.Context().SetUserValue("tid", tid)
+		c.Append("X-TID", tid)
+
+		// Continue stack
+		return c.Next()
+	}
+}

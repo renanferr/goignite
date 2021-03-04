e8bdd82424fa4e3ee45fbf65479342e5270b4738
João Paulo Faria <jpfaria@gmail.com>
2021-03-04 04:26:06 -0300

    fix(http): fixing default config

--- http/v1/server/config.go
+++ http/v1/server/config.go
@@ -1,6 +1,10 @@
 package gihttp
 
-import giconfig "github.com/b2wdigital/goignite/v2/config"
+import (
+	"time"
+
+	giconfig "github.com/b2wdigital/goignite/v2/config"
+)
 
 const (
 	root              = "gi.http.server"
@@ -13,10 +17,34 @@ const (
 )
 
 func init() {
-	giconfig.Add(serverAddress, ":8080", "server address")
+	giconfig.Add(serverAddress, ":8081", "server address")
 	giconfig.Add(maxHeaderBytes, 1048576, "max header timeout")
-	giconfig.Add(readHeaderTimeout, "1s", "read header timeout")
-	giconfig.Add(readTimeout, "1", "read timeout")
-	giconfig.Add(writeTimeout, "7s", "write timeout ")
-	giconfig.Add(idleTimeout, "10m", "interval between question creation")
+	giconfig.Add(readHeaderTimeout, 1*time.Second, "read header timeout")
+	giconfig.Add(readTimeout, 1*time.Second, "read timeout")
+	giconfig.Add(writeTimeout, 7*time.Second, "write timeout ")
+	giconfig.Add(idleTimeout, 30*time.Second, "idle timeout")
+}
+
+func GetServerAddress() string {
+	return giconfig.String(serverAddress)
+}
+
+func GetMaxHeaderBytes() int {
+	return giconfig.Int(maxHeaderBytes)
+}
+
+func GetReadHeaderTimeout() time.Duration {
+	return giconfig.Duration(readHeaderTimeout)
+}
+
+func GetReadTimeout() time.Duration {
+	return giconfig.Duration(readTimeout)
+}
+
+func GetWriteTimeout() time.Duration {
+	return giconfig.Duration(writeTimeout)
+}
+
+func GetIdleTimeout() time.Duration {
+	return giconfig.Duration(idleTimeout)
 }

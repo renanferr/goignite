873fc083c9fff8293df35434b173a7ded9651dad
João Paulo Faria <jpfaria@gmail.com>
2021-03-16 17:30:12 -0300

    fix(datadog): fixes feature flag

--- datadog/v1/tracer.go
+++ datadog/v1/tracer.go
@@ -18,79 +18,79 @@ var once sync.Once
 func NewTracer(ctx context.Context, opts ...tracer.StartOption) {
 
 	if !IsEnabled() {
-
-		once.Do(func() {
-
-			logger := gilog.FromContext(ctx)
-
-			svc := giconfig.String(service)
-			if v := giinfo.AppName; v != "" {
-				svc = v
-			}
-			if v := os.Getenv("DD_SERVICE"); v != "" {
-				svc = v
-			}
-
-			host := giconfig.String(host)
-			if v := os.Getenv("DD_AGENT_HOST"); v != "" {
-				host = v
-			}
-
-			port := giconfig.String(port)
-			if v := os.Getenv("DD_TRACE_AGENT_PORT"); v != "" {
-				port = v
-			}
-
-			env := giconfig.String(env)
-			if v := os.Getenv("DD_ENV"); v != "" {
-				env = v
-			}
-
-			var version string
-			if v := giinfo.Version; v != "" {
-				version = v
-			}
-			if v := os.Getenv("DD_VERSION"); v != "" {
-				version = v
-			}
-
-			addr := net.JoinHostPort(host, port)
-
-			httpClientOpt := &gihttp.Options{}
-
-			err := giconfig.UnmarshalWithPath(httpClientRoot, httpClientOpt)
-			if err != nil {
-				logger.Panic(err)
-			}
-
-			httpClient := gihttp.NewClient(ctx, httpClientOpt)
-
-			startOptions := []tracer.StartOption{
-				tracer.WithAgentAddr(addr),
-				tracer.WithEnv(env),
-				tracer.WithService(svc),
-				tracer.WithServiceVersion(version),
-				tracer.WithLogger(NewLogger()),
-				tracer.WithHTTPClient(httpClient),
-				tracer.WithAnalytics(giconfig.Bool(analytics)),
-				tracer.WithAnalyticsRate(giconfig.Float64(analyticsRate)),
-				tracer.WithLambdaMode(giconfig.Bool(lambdaMode)),
-				tracer.WithDebugMode(giconfig.Bool(debugMode)),
-				tracer.WithDebugStack(giconfig.Bool(debugStack)),
-			}
-
-			for k, v := range giconfig.StringMap(tags) {
-				startOptions = append(startOptions, tracer.WithGlobalTag(k, v))
-			}
-
-			startOptions = append(startOptions, opts...)
-
-			tracer.Start(startOptions...)
-
-			logger.Infof("started a datadog tracer: %s", svc)
-
-		})
-
+		return
 	}
+
+	once.Do(func() {
+
+		logger := gilog.FromContext(ctx)
+
+		svc := giconfig.String(service)
+		if v := giinfo.AppName; v != "" {
+			svc = v
+		}
+		if v := os.Getenv("DD_SERVICE"); v != "" {
+			svc = v
+		}
+
+		host := giconfig.String(host)
+		if v := os.Getenv("DD_AGENT_HOST"); v != "" {
+			host = v
+		}
+
+		port := giconfig.String(port)
+		if v := os.Getenv("DD_TRACE_AGENT_PORT"); v != "" {
+			port = v
+		}
+
+		env := giconfig.String(env)
+		if v := os.Getenv("DD_ENV"); v != "" {
+			env = v
+		}
+
+		var version string
+		if v := giinfo.Version; v != "" {
+			version = v
+		}
+		if v := os.Getenv("DD_VERSION"); v != "" {
+			version = v
+		}
+
+		addr := net.JoinHostPort(host, port)
+
+		httpClientOpt := &gihttp.Options{}
+
+		err := giconfig.UnmarshalWithPath(httpClientRoot, httpClientOpt)
+		if err != nil {
+			logger.Panic(err)
+		}
+
+		httpClient := gihttp.NewClient(ctx, httpClientOpt)
+
+		startOptions := []tracer.StartOption{
+			tracer.WithAgentAddr(addr),
+			tracer.WithEnv(env),
+			tracer.WithService(svc),
+			tracer.WithServiceVersion(version),
+			tracer.WithLogger(NewLogger()),
+			tracer.WithHTTPClient(httpClient),
+			tracer.WithAnalytics(giconfig.Bool(analytics)),
+			tracer.WithAnalyticsRate(giconfig.Float64(analyticsRate)),
+			tracer.WithLambdaMode(giconfig.Bool(lambdaMode)),
+			tracer.WithDebugMode(giconfig.Bool(debugMode)),
+			tracer.WithDebugStack(giconfig.Bool(debugStack)),
+		}
+
+		for k, v := range giconfig.StringMap(tags) {
+			startOptions = append(startOptions, tracer.WithGlobalTag(k, v))
+		}
+
+		startOptions = append(startOptions, opts...)
+
+		tracer.Start(startOptions...)
+
+		logger.Infof("started a datadog tracer: %s", svc)
+
+	})
 
 }

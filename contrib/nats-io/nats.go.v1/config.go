dc8bf2d3a09e3772cf5189f405d83c5080f1778e
João Paulo Faria <jpfaria@gmail.com>
2021-04-07 00:59:52 -0300

    fix(nats): fixes config

--- contrib/nats-io/nats.go.v1/config.go
+++ contrib/nats-io/nats.go.v1/config.go
@@ -8,19 +8,15 @@ import (
 )
 
 const (
-	root            = "gi.nats"
-	maxReconnects   = root + ".maxReconnects"
-	reconnectWait   = root + ".reconnectWait"
-	url             = root + ".url"
-	NewRelicEnabled = root + ".newrelic.enabled"
-	ExtRoot         = root + ".ext"
+	root          = "gi.nats"
+	maxReconnects = root + ".maxReconnects"
+	reconnectWait = root + ".reconnectWait"
+	url           = root + ".url"
+	ExtRoot       = root + ".ext"
 )
 
 func init() {
-
 	config.Add(maxReconnects, 1000, "define max reconnects to nats server")
 	config.Add(reconnectWait, 1*time.Second, "define reconnects waiting before reconnect to nats server")
 	config.Add(url, nats.DefaultURL, "define nats server url")
-	config.Add(NewRelicEnabled, false, "enable/disable newrelic")
-
 }

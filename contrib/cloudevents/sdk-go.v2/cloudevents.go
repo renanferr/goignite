be5e9865161537a43a50a7c2a4db83736c7e6064
João Paulo Faria <jpfaria@gmail.com>
2021-04-09 11:24:27 -0300

    feat(cloudevents): refactor client

--- contrib/cloudevents/sdk-go.v2/cloudevents.go
+++ contrib/cloudevents/sdk-go.v2/cloudevents.go
@@ -8,21 +8,21 @@ import (
 	"github.com/cloudevents/sdk-go/v2/client"
 )
 
-type Server struct {
+type Client struct {
 	handler Handler
 	client  client.Client
 }
 
-func NewDefault(ctx context.Context, handler Handler) *Server {
+func NewDefaultClient(ctx context.Context, handler Handler) *Client {
 	logger := log.FromContext(ctx)
 	c, err := v2.NewDefaultClient()
 	if err != nil {
 		logger.Panic(err.Error())
 	}
-	return &Server{handler: handler, client: c}
+	return &Client{handler: handler, client: c}
 }
 
-func (s *Server) Serve(ctx context.Context) {
+func (s *Client) Start(ctx context.Context) {
 	logger := log.FromContext(ctx).WithTypeOf(*s)
 	if err := s.client.StartReceiver(ctx, s.handler); err != nil {
 		logger.Panic(err.Error())

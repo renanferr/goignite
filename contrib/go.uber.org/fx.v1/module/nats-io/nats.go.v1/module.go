8c3887a56adf15d9b64517f41e72ea9ba0ef5aec
João Paulo Faria <jpfaria@gmail.com>
2021-04-12 22:31:45 -0300

    feat(fx/nats): adds publisher module

--- contrib/go.uber.org/fx.v1/module/nats-io/nats.go.v1/module.go
+++ contrib/go.uber.org/fx.v1/module/nats-io/nats.go.v1/module.go
@@ -4,20 +4,37 @@ import (
 	"sync"
 
 	contextfx "github.com/b2wdigital/goignite/v2/contrib/go.uber.org/fx.v1/module/context"
-	ginats "github.com/b2wdigital/goignite/v2/contrib/nats-io/nats.go.v1"
+	"github.com/b2wdigital/goignite/v2/contrib/nats-io/nats.go.v1"
 	"go.uber.org/fx"
 )
 
 var subsOnce sync.Once
 
-func Module() fx.Option {
+func SubscriberModule() fx.Option {
 	options := fx.Options()
 
 	subsOnce.Do(func() {
 		options = fx.Options(
 			contextfx.Module(),
 			fx.Provide(
-				ginats.NewDefaultSubscriber,
+				nats.NewDefaultSubscriber,
+			),
+		)
+	})
+
+	return options
+}
+
+var pubOnce sync.Once
+
+func PublisherModule() fx.Option {
+	options := fx.Options()
+
+	pubOnce.Do(func() {
+		options = fx.Options(
+			contextfx.Module(),
+			fx.Provide(
+				nats.NewDefaultPublisher,
 			),
 		)
 	})
